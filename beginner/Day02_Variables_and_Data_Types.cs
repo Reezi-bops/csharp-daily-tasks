7e4465404420bb5db62125f815a8f460826fc1c9
Raiko Estrada <jones.estrada3@gmail.com>
2025-08-12 01:23:19 +0800

    day2

--- beginner/Day02_Variables_and_Data_Types.cs
+++ beginner/Day02_Variables_and_Data_Types.cs
@@ -6,24 +6,16 @@ namespace Beginner
         static void Main(string[] args)
         {
             int age = 19;
-
             double height = 5.7;
-
             char initial = 'R';
-
             string name = "Raiko";
-
             bool isStudent = true;
 
 
             Console.WriteLine("Name: " + name);
-
             Console.WriteLine("Age: " + age);
-
             Console.WriteLine("Height: " + height);
-
             Console.WriteLine("Initial: " + initial);
-            
             Console.WriteLine("Is Student: " + isStudent);
 
         }
